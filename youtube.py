6e1f381adcafd8d893d6765c541d3462b3c1e5da
ju57u5 <geldtransporter@hotmail.de>
2015-08-13 13:43:57 +0200

    Update to python 3

--- youtube.py
+++ youtube.py
@@ -1,18 +1,25 @@
 import time
 import datetime
-import cookielib
-import urllib2
+import urllib.request
 import wget
 import sys
-import urlparse
+from urllib.parse import urlparse
 import os
 import argparse
 import re
 
+def getPath():
+	try:
+		approot = os.path.dirname(os.path.abspath(__file__))
+	except NameError:  # We are the main py2exe script, not a module
+		approot = os.path.dirname(os.path.abspath(sys.argv[0]))
+	return approot
+
+
 def openUrl(url):
 	LoginHeader = {"User-Agent": "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.2.8) Gecko/20100722 Firefox/3.6.8 GTB7.1 (.NET CLR 3.5.30729)"}
-	req = urllib2.Request(url)
-	response = urllib2.urlopen(req)
+	req = urllib.request.Request(url)
+	response = urllib.request.urlopen(req)
 	html = response.read()
 	return html
 
@@ -30,7 +37,7 @@ def stringbetween(string,startstr,endstr):
 	end = string.find(endstr,start+len(startstr))
 	return string[start+len(startstr):end]
 
-def downloadvideo(url,name,vformat,path=os.path.dirname(os.path.realpath(__file__))+ "\\download\\"):
+def downloadvideo(url,name,vformat,path=getPath() + "\\download\\"):
 	html = openUrl('http://convert2mp3.net/c-'+vformat+'.php?url='+url)
 	convertbefehl = stringbetween(html,'convert(',');')
 
@@ -51,16 +58,16 @@ def downloadvideo(url,name,vformat,path=os.path.dirname(os.path.realpath(__file_
 	html2 = '1'
 	while not "3" in html2 :
 		html2 = openUrl('http://convert2mp3.net/status.php?id='+vid+'&key='+key+'&cs='+cs+'&time='+str(unixtime()))
-		print ''
+		print ("")
 		print('Converting...')
 		time.sleep(1)
-	print 'Downloading...'
+	print ('Downloading...')
 	downloadurl='http://cdl'+cs+'.convert2mp3.net/download.php?id='+vid+'&key='+key
 
 	if not os.path.exists(path):
 	    os.makedirs(path)
 	if name!='none': 
-		filename = os.path.dirname(os.path.realpath(__file__))+ "\\download\\" + name +"."+vformat
+		filename = getPath() + "\\download\\" + name +"."+vformat
 		wget.download(downloadurl,filename)
 	else :
 		wget.download(downloadurl,path)
@@ -70,7 +77,7 @@ def getVideoID(url):
 	url_data = urlparse.urlparse(url)
 	query = urlparse.parse_qs(url_data.query)
 	if not "v" in query:
-		print 'Video Url is not valid!'
+		print ('Video Url is not valid!')
 		sys.exit(1)
 	video = query["v"][0]
 	return video
@@ -90,7 +97,7 @@ def getPlaylistVideos(url):
 	for i,entry in enumerate(links):
 		links[i] = 'http://www.youtube.com/watch?v=' + stringbetween(entry,'watch?v=','&amp;')
 	if not links:
-		print 'Couldnt read Playlist. Try again!'
+		print ('Couldnt read Playlist. Try again!')
 		sys.exit(1)
 	return unique(links)
 
@@ -122,12 +129,12 @@ aparser.add_argument('-pl', '--playlist', dest='playlist', action="store_true", 
 aparser.add_argument('-no', '--startno', dest='startno', action="store", type=int, default=1, help='starting number of naming, if downloading playlist')
 aparser.add_argument('-s', '--start', dest='start', action="store", type=int, default=1, help='starting number, if downloading playlist')
 aparser.add_argument('-e', '--end', dest='end', action="store", type=int, default=1000, help='ending number, if downloading playlist')
+aparser.add_argument('-o', '--output', dest='out', action="store", help='output file/directory')
+
 
 args = aparser.parse_args()
 
 if args.playlist:
 	dlplaylist(args)
-#elif (args.name == 'none'):
-#	downloadvideo(args.videourl,getVideoID(args.videourl),args.format)
 else:
 	downloadvideo(args.videourl,args.name,args.format)
